484940a513f53e30ae22d00141eb3c1744ed99ee
JohannSK1 <johannkarim30@gmail.com>
2023-03-21 10:20:23 +0800

    initial commit

---
+++ indexActivity.js
@@ -0,0 +1,19 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = 'Johann';
+let lastName = 'Karim';
+let age = '20';
+let hobbies = ['videogames, music, netflix, anime, sleeping'];
+let homeAdress = { 
+houseNumber: 'BLK 24, Lot 3, Phase 2', 
+street: 'Omboy', 
+subdivision: 'Spring Town Villas', 
+barangay: 'Bucal',
+city: 'Tanza',
+province: 'Cavite',
+zipCode: '4108'};
+
+console.log(firstName, lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAdress);
